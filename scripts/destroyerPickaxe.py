2abff9fc61daf467c98b9fb9d8f546f2ed7190d5
Taranis <solovyov.production@gmail.com>
2023-01-14 18:29:28 +0300

    Added logs

--- scripts/destroyerPickaxe.py
+++ scripts/destroyerPickaxe.py
@@ -11,8 +11,8 @@ class DestroyerPickAxe(BaseScript):
         self.name = "destroyerPickaxe"
         self.keys = self.keys_data[self.name]
         self.keyActivate = self.keys["activate_key"]
-
-        self.attacks_count = 234  # 1866  933  467  234
+        self.full_attacks_count = int(self.keys["key10"]["value"])
+        self.attacks_count = int(round(self.full_attacks_count / 8))  # 1866  933  467  234
         self.weapon_slot = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+1", "+2", "+3", "+4", "+5", "+6", "+7", "+8", "+9", "+0"]  #
         self.started = False
         self.attack_overhead = self.keys["key1"]["value"]
@@ -24,11 +24,15 @@ class DestroyerPickAxe(BaseScript):
         self.after_attack_time = float(self.keys["key7"]["value"])
         self.backward_walking_time = float(self.keys["key8"]["value"])
         self.forward_walking_time = float(self.keys["key9"]["value"])
+        self.attack_actually_count_all = 0
+        self.actually_weapon = ""
+
 
     def master_action(self):
         if not self.started:
             self.started = True
             for key in self.weapon_slot:
+                self.actually_weapon = key
                 self.weapon_changer(key)
                 self.attack_and_antiafk()
         self.jumping()
@@ -72,7 +76,12 @@ class DestroyerPickAxe(BaseScript):
             if self.isStop or self.exitKey:
                 break
             self.attacker()
+            self.attack_actually_count_all += 1
             count += 1
+            print(f"Кирка клавиши: {self.actually_weapon}")
+            print(f"Осталось ударов в цикле: {self.attacks_count - count}")
+            print(f"Осталось ударов всего: {(self.attacks_count * 8) - self.attack_actually_count_all}")
+            print()
 
     def attack_and_antiafk(self):
 
@@ -87,9 +96,7 @@ class DestroyerPickAxe(BaseScript):
         if not self.isStop and not self.exitKey:
             self.hold_and_release_wait(self.attack_overhead, self.hold_attack_time)
             time.sleep(self.after_attack_time)
-            print("attacker")
-            print(self.keyActivate)
-            print(self.attack_overhead)
+            print()
 
     def weapon_changer(self, key):
         self.wait(1)

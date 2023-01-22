3bb0cd4c7bd52a75e95a4c66e1ca70c8aaa181c0
Taranis <solovyov.production@gmail.com>
2023-01-22 03:07:16 +0300

    minor refactoring of attackers script

--- scripts/attacker.py
+++ scripts/attacker.py
@@ -22,6 +22,7 @@ class Attacker(BaseScript):
 
         self.side_attack = []
         self.move_keys = []
+        self.actually_attack = None
         match self.mode:
             case '1':
                 self.side_attack = [self.left]
@@ -37,51 +38,75 @@ class Attacker(BaseScript):
                 self.side_attack = [self.left, self.right]
                 self.move_keys = [self.overhead, self.down, self.side_attack]
 
-    def attacks(self):
+    def feint_deep_random_to_random(self):
+        for _ in range(random.choice([2, 3, 4, 3, 4, 5])):
+            self.choose_attack()
+
+            self.hold(self.actually_attack)
+            self.wait(0.2)
+            self.press(self.feint)
+            self.release(self.actually_attack)
+            self.wait(0.01)
+
         attack = random.choice(self.move_keys)
-
-        if attack == self.down:
-            attack = random.choice(self.move_keys)
-
         if attack == self.side_attack:
             attack = random.choice(self.side_attack)
+        self.press(attack)
 
-        self.hold_and_release_wait(attack, self.charging)
+    def choose_attack(self):
+        self.actually_attack = random.choice(self.move_keys)
+        if self.actually_attack == self.side_attack:
+            self.actually_attack = random.choice(self.side_attack)
+
+    def feint_random_to_random(self):
+        self.choose_attack()
+
+        self.hold(self.actually_attack)
+        self.wait(0.2)
+        self.press(self.feint)
+        self.release(self.actually_attack)
+        self.wait(0.01)
+
+        self.choose_attack()
+        self.press(self.actually_attack)
+
+    def feint_over_to_side(self):
+        side_attack = random.choice(self.side_attack)
+        self.hold(self.overhead)
+        self.wait(0.3)
+        self.press(self.feint)
+        self.release(self.overhead)
+        self.wait(0.02)
+        self.press(side_attack)
+        self.wait(0.01)
+
+    def feint_side_to_over(self):
+        side_attack = random.choice(self.side_attack)
+        self.hold(side_attack)
+        self.wait(0.2)
+        self.press(self.feint)
+        self.release(side_attack)
+        self.wait(0.01)
+        self.press(self.overhead)
+
+    def attacks(self):
+        self.choose_attack()
+
+        if self.actually_attack == self.down:
+            self.choose_attack()
+
+        self.hold_and_release_wait(self.actually_attack, self.charging)
         self.wait(self.timing)
 
     def feints_attacks(self):
         rnd = random.choice([1, 2, 3])
         if rnd != 1:
-            attack = random.choice(self.move_keys)
-
-            if attack == self.down:
-                attack = random.choice(self.move_keys)
-
-            if attack == self.side_attack:
-                attack = random.choice(self.side_attack)
-
-            self.hold_and_release_wait(attack, self.charging)
-            self.wait(self.timing)
+            self.attacks()
         else:
-
-            rnd = random.choice([1, 2])
-            if rnd == 1:
-                side_attack = random.choice(self.side_attack)
-                self.hold(side_attack)
-                self.wait(0.2)
-                self.press(self.feint)
-                self.release(side_attack)
-                self.wait(0.01)
-                self.press(self.overhead)
-            else:
-                side_attack = random.choice(self.side_attack)
-                self.hold(self.overhead)
-                self.wait(0.3)
-                self.press(self.feint)
-                self.release(self.overhead)
-                self.wait(0.02)
-                self.press(side_attack)
-                self.wait(0.01)
+            random.choice([self.feint_side_to_over,
+                           self.feint_random_to_random,
+                           self.feint_over_to_side,
+                           self.feint_deep_random_to_random])()
 
     def custom(self):
         if self.feint_bool == "1":
